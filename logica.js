1446636de8677a62aff8ca67e0bd18e323fef1c3
esdrascarvalho-45 <57711029+esdrascarvalho-45@users.noreply.github.com>
2024-05-10 19:53:44 -0300

    adicionado arguivo js e alterando html

---
+++ logica.js
@@ -0,0 +1,19 @@
+function clicar(){
+    let um=document.querySelector(".um"); 
+     
+    let dois=um.querySelector(".dois");
+     
+    dois.children[0].innerHTML += " alet"
+    dois.children[1].append(" (2°)");
+    
+    let v=document.createElement("li");
+     
+    v.innerText="mais um";
+    
+    dois.appen(v)
+ }
+ let va=document.querySelector(".bt");
+ 
+ va.addEventListener("click",function(){
+   clicar();
+ })
